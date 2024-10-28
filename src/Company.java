df98cee9ad44b7a2efd7c7022d806a68b40b5a88
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2024-10-28 22:45:53 +0300

    Map to track departments

--- src/Company.java
+++ src/Company.java
@@ -1,2 +1,37 @@
+import java.util.*;
+
 public class Company {
+    private String name;
+//    private List<Department> departments = new ArrayList<>();
+    private Map<String, Set<Employee>> departmentEmployeeMap = new HashMap<>();
+
+    public Company(){
+    }
+
+    public void addEmployeeToDepartment(String department, Employee employee){
+
+    }
+
+
+//    public void addEmployeeToDepartment(Employee employee, Department department){
+//        for (Department department1 : departments) {
+//            if (department1.equals(department)){
+//                department1.addEmployee(employee);
+//            }
+//        }
+//    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Company company = (Company) o;
+        return Objects.equals(name, company.name) && Objects.equals(departments, company.departments);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, departments);
+    }
 }
--- src/Department.java
+++ src/Department.java
@@ -2,6 +2,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Department {
+
     private String name;
     private Set<Employee> employeeSet = new HashSet<>();
 
@@ -10,15 +11,19 @@ public class Department {
     }
 
     public void printEmployeeSet(){
+
         if (employeeSet.isEmpty()){
             System.out.println("There is no employee");
             return;
-        }
-        System.out.println("Print Employees: ");
-        for (Employee employee : employeeSet) {
-            System.out.println(employee);
+        } else {
+            System.out.println("Print Employees: ");
+            for (Employee employee : employeeSet) {
+                System.out.println(employee);
+            }
+
         }
     }
+
     // add method
     public void addEmployee(Employee employee){
         employeeSet.add(employee);
