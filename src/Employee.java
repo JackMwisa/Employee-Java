30a9c9155c8d08b1c7d81f5647abc311273f3a5a
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2024-10-28 21:28:50 +0300

    Getters and setters

--- src/Employee.java
+++ src/Employee.java
@@ -2,4 +2,12 @@
 public class Employee {
     private String name;
     private int id;
+
+    // constuctors to create objs of the class
+    public Employee(String name, int age){
+        this.name = name;
+        this.id = id;
+    }
+
+
 }
